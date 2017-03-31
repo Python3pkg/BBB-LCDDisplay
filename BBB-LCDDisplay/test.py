4286a5520d32c6b2e320cb90d4aa70f0c16fca4e
JonathanPetit <petit.jonathan16@gmail.com>
2017-03-31 13:51:36 +0200

    pypi librairie available

--- BBB-LCDDisplay/test.py
+++ BBB-LCDDisplay/test.py
@@ -2,6 +2,8 @@ from servo import *
 from i2c import *
 from gpio import *
 
+import time
+
 def test_gpio():
 	pin44 = BBB_GPIO("P8_44")
 	pin44.direction(OUTPUT)
@@ -19,7 +21,17 @@ def test_i2c():
 	a.write('a')
 	a.write(0x01)
 
+def test_blinkled():
+	pin44 = BBB_GPIO("P8_44")
+	pin44.direction(OUTPUT)
+	while True:
+		pin44.value(HIGH)
+		time.sleep(0.5)
+		pin44.value(LOW)
+
+
 if __name__ == '__main__':
-	test_gpio()
-	test_i2c()
-	test_servo()
+	#test_gpio()
+	#test_i2c()
+	#test_servo()
+	test_blinkled()
